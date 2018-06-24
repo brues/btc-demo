26358d67d3ccee779c960356a6151b5b9a557190
blues <guoyb1990@163.com>
2018-06-25 00:40:49 +0800

    启动类

---
+++ src/main/java/btcdemo/btcdemo/BtcDemoApplication.java
@@ -0,0 +1,17 @@
+package btcdemo.btcdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ *
+ * @author blues
+ */
+@SpringBootApplication
+public class BtcDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BtcDemoApplication.class, args);
+	}
+}
